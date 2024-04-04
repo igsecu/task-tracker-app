920f55b9759743c67a238f05d23cf4a2b5fade95
Ignacio Cunial <igsecu@hotmail.com>
2024-04-04 17:44:58 -0300

    Imported Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   return (
     <div className="navbar navbar-expand-lg bg-dark navbar-dark">
       <div className="container">
         <h1 className="navbar-brand">{title}</h1>
+        <Button text="ADD TASK" />
       </div>
     </div>
   );
