580a835a4432c9612ce0063e9d80318aa59f1b65
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 16:28:54 -0300

    Changed button color if showAddTask

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Button = ({ text }) => {
-  const onClick = (e) => {
-    console.log("CLICK");
-  };
-
+const Button = ({ text, onAdd, showAddTask }) => {
   return (
-    <button type="button" className="btn btn-light px-2 py-1" onClick={onClick}>
+    <button
+      type="button"
+      className={`btn ${showAddTask ? "btn-danger" : "btn-success"} px-2 py-1`}
+      onClick={onAdd}
+    >
       {text}
     </button>
   );
