aa88bf6ef03214d3dc51f6297d99f002c8674483
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 15:02:24 -0300

    Added onToggle prop to Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,15 @@
 import Task from "./Task";
 
-function Tasks({ tasks, onDelete }) {
+function Tasks({ tasks, onDelete, onToggle }) {
   return (
     <div className="container d-flex flex-column align-items-center">
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} />
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={() => onToggle(task.id)}
+        />
       ))}
     </div>
   );
