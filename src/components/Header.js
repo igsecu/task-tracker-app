04f1bf8f4649e0645ef7017ee88f701dc3360f01
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 16:28:37 -0300

    Changed button text if showAddTask

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAddTask }) => {
   return (
     <div className="navbar bg-dark mb-3" data-bs-theme="dark">
       <div className="container">
         <h1 className="navbar-brand">{title}</h1>
 
-        <Button text="ADD TASK" />
+        <Button
+          text={`${showAddTask ? "CLOSE" : "ADD TASK"}`}
+          onAdd={onAdd}
+          showAddTask={showAddTask}
+        />
       </div>
     </div>
   );
