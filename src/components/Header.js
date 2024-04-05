238217243bc97dde97b6baf50debfc4070f04d38
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 14:45:55 -0300

    Added margin bottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@ import Button from "./Button";
 
 const Header = ({ title }) => {
   return (
-    <div className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div className="navbar bg-dark mb-3" data-bs-theme="dark">
       <div className="container">
         <h1 className="navbar-brand">{title}</h1>
+
         <Button text="ADD TASK" />
       </div>
     </div>
