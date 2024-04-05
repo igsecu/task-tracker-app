75a67f60808087f576103a7cf7b7ef6e41de0d3b
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 14:47:05 -0300

    Added onDelete function to FaTimes icon onClick

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import { FaTimes } from "react-icons/fa";
 
-function Task({ task }) {
+function Task({ task, onDelete }) {
   return (
     <div className="mb-3 border-bottom border-secondary-subtle w-75">
       <div className="d-flex w-100 justify-content-between align-items-center">
         <h5 className="mb-1">{task.text}</h5>
-        <FaTimes style={{ cursor: "pointer", color: "red" }} />
+        <FaTimes
+          style={{ cursor: "pointer", color: "red" }}
+          onClick={() => onDelete(task.id)}
+        />
       </div>
       <p className="mb-1">{task.day}</p>
     </div>
