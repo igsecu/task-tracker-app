bb22b5e6a19f1bc13cb50acca43a2b91a25d9f63
Ignacio Cunial <igsecu@hotmail.com>
2024-04-05 15:03:11 -0300

    Added onToggle function to DIV container and added style if reminder is true

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,26 @@
 import { FaTimes } from "react-icons/fa";
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, onToggle }) {
   return (
-    <div className="mb-3 border-bottom border-secondary-subtle w-75">
-      <div className="d-flex w-100 justify-content-between align-items-center">
-        <h5 className="mb-1">{task.text}</h5>
-        <FaTimes
-          style={{ cursor: "pointer", color: "red" }}
-          onClick={() => onDelete(task.id)}
-        />
+    <div
+      className="mb-3 border-bottom border-secondary-subtle w-75"
+      onDoubleClick={onToggle}
+      style={{ cursor: "pointer" }}
+    >
+      <div
+        className={`${
+          task.reminder ? "border-start border-5 border-success ps-2" : ""
+        }`}
+      >
+        <div className="d-flex w-100 justify-content-between align-items-center">
+          <h5 className="mb-1">{task.text}</h5>
+          <FaTimes
+            style={{ cursor: "pointer", color: "red" }}
+            onClick={() => onDelete(task.id)}
+          />
+        </div>
+        <p className="mb-1">{task.day}</p>
       </div>
-      <p className="mb-1">{task.day}</p>
     </div>
   );
 }
